b1d31c248447811e24d31bdd5ad89c1af5945675
abdallah-zaghloul <61375797+abdallah-zaghloul@users.noreply.github.com>
2021-01-29 15:13:47 +0200

    Add files via upload

---
+++ slack-notifier/slack-notifier.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: Slack Notifier
+* Plugin URI: https://github.com/abdallah-zaghloul/slack-notifier.git
+* Description: Integrate with slack.com (Slack Auth - Create Channel - Fetch All Workspace Channels)
+* Author: Abdallah Zaghloul
+* Version: 1.0.0
+* Author URI: https://www.linkedin.com/in/abdallah-zaghloul/
+**/
